6d17dd6a30bb14c6acc162766496132693bbb39c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:18:38 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,10 @@
+return {
+    ["%1 to play  B:%2 W:%3"]                    = { fr = "%1 à jouer  N:%2 B:%3" },
+    ["%1 wins! Black: %2  White: %3"]             = { fr = "%1 gagne ! Noir : %2  Blanc : %3" },
+    ["Black"]                                     = { fr = "Noir" },
+    ["Board size"]                                = { fr = "Taille du plateau" },
+    ["Classic territory and capture board game."] = { fr = "Jeu de plateau classique de territoire et de capture." },
+    ["Go"]                                        = { fr = "Go" },
+    ["Pass"]                                       = { fr = "Passer" },
+    ["White"]                                     = { fr = "Blanc" },
+}
