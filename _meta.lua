85c35f1887342d73011dc2ef71859884d435688b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-28 13:31:02 +0200

    chore: bump version to 1.0.4

--- _meta.lua
+++ _meta.lua
@@ -2,7 +2,7 @@ local _ = require("gettext")
 
 return {
     name        = "go",
-    version     = "1.0.3",
+    version     = "1.0.4",
     fullname    = _("Go"),
     description = _("Classic territory and capture board game."),
 }
