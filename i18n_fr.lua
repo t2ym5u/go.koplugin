1753bc72b69ab604897d10fdf2dd895d593720ae
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:58:46 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,10 +1,10 @@
 return {
-    ["%1 to play  B:%2 W:%3"]                    = { fr = "%1 à jouer  N:%2 B:%3" },
-    ["%1 wins! Black: %2  White: %3"]             = { fr = "%1 gagne ! Noir : %2  Blanc : %3" },
-    ["Black"]                                     = { fr = "Noir" },
-    ["Board size"]                                = { fr = "Taille du plateau" },
-    ["Classic territory and capture board game."] = { fr = "Jeu de plateau classique de territoire et de capture." },
-    ["Go"]                                        = { fr = "Go" },
-    ["Pass"]                                       = { fr = "Passer" },
-    ["White"]                                     = { fr = "Blanc" },
+    ["%1 to play  B:%2 W:%3"]                    = { fr = "%1 à jouer  N:%2 B:%3", es = "%1 a jugar  N:%2 B:%3", de = "%1 am Zug  S:%2 W:%3" },
+    ["%1 wins! Black: %2  White: %3"]             = { fr = "%1 gagne ! Noir : %2  Blanc : %3", es = "¡%1 gana! Negro: %2  Blanco: %3", de = "%1 gewinnt! Schwarz: %2  Weiß: %3" },
+    ["Black"]                                     = { fr = "Noir", es = "Negro", de = "Schwarz" },
+    ["Board size"]                                = { fr = "Taille du plateau", es = "Tamaño del tablero", de = "Brettgröße" },
+    ["Classic territory and capture board game."] = { fr = "Jeu de plateau classique de territoire et de capture.", es = "Juego de mesa clásico de territorio y captura.", de = "Klassisches Brettspiel um Gebiet und Gefangene." },
+    ["Go"]                                        = { fr = "Go", es = "Go", de = "Go" },
+    ["Pass"]                                       = { fr = "Passer", es = "Pasar", de = "Passen" },
+    ["White"]                                     = { fr = "Blanc", es = "Blanco", de = "Weiß" },
 }
